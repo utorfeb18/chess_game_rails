6db17b90aae9d94c6667d12175c76a2ed1cbacb8
Fil Gambatesa <fil.gambatesa@gmail.com>
2019-08-09 17:32:00 -0400

    Updates seeds.rb with starting values for Pieces table.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Seed data for the starting position of the chess pieces
+
+Piece.create([
+                 {piece: 'Rook', starting_location: "A1", color: "white"},
+                 {piece: 'Knight', starting_location: "B1", color: "white"},
+                 {piece: 'Bishop', starting_location: "C1", color: "white"},
+                 {piece: 'Queen', starting_location: "D1", color: "white"},
+                 {piece: 'King', starting_location: "E1", color: "white"},
+                 {piece: 'Bishop', starting_location: "F1", color: "white"},
+                 {piece: 'Knight', starting_location: "G1", color: "white"},
+                 {piece: 'Rook', starting_location: "H1", color: "white"},
+                 {piece: 'Pawn', starting_location: "A2", color: "white"},
+                 {piece: 'Pawn', starting_location: "B2", color: "white"},
+                 {piece: 'Pawn', starting_location: "C2", color: "white"},
+                 {piece: 'Pawn', starting_location: "D2", color: "white"},
+                 {piece: 'Pawn', starting_location: "E2", color: "white"},
+                 {piece: 'Pawn', starting_location: "F2", color: "white"},
+                 {piece: 'Pawn', starting_location: "G2", color: "white"},
+                 {piece: 'Pawn', starting_location: "H2", color: "white"},
+                 {piece: 'Rook', starting_location: "A8", color: "black"},
+                 {piece: 'Knight', starting_location: "B8", color: "black"},
+                 {piece: 'Bishop', starting_location: "C8", color: "black"},
+                 {piece: 'Queen', starting_location: "D8", color: "black"},
+                 {piece: 'King', starting_location: "E8", color: "black"},
+                 {piece: 'Bishop', starting_location: "F8", color: "black"},
+                 {piece: 'Knight', starting_location: "G8", color: "black"},
+                 {piece: 'Rook', starting_location: "H8", color: "black"},
+                 {piece: 'Pawn', starting_location: "A7", color: "black"},
+                 {piece: 'Pawn', starting_location: "B7", color: "black"},
+                 {piece: 'Pawn', starting_location: "C7", color: "black"},
+                 {piece: 'Pawn', starting_location: "D7", color: "black"},
+                 {piece: 'Pawn', starting_location: "E7", color: "black"},
+                 {piece: 'Pawn', starting_location: "F7", color: "black"},
+                 {piece: 'Pawn', starting_location: "G7", color: "black"},
+                 {piece: 'Pawn', starting_location: "H7", color: "black"}
+                          ])
